ea87112469753fa1068a1ed10739adeb09d7f000
Victor Cabral <victor_cabral_o@hotmail.com>
2023-06-22 16:40:00 -0300

    feat: Pirate Domain

---
+++ src/database/Domains/Pirate.ts
@@ -0,0 +1,33 @@
+import IPirate from '../../Interfaces/IPirate';
+
+class Pirate {
+  protected name: string;
+  protected alias?: string;
+  protected crew: string;
+  protected bounty: number;
+  protected akumaNoMi?: string | undefined;
+  protected haki?: string[] | undefined;
+  protected weapons?: string | undefined;
+  protected age: number;
+  protected gender: string;
+  protected height: string;
+  protected personality: string;
+  protected goal: string;
+  
+  constructor(pirate: IPirate) {
+    this.name = pirate.name;
+    this.alias = pirate.alias;
+    this.crew = pirate.crew;
+    this.bounty = pirate.bounty;
+    this.akumaNoMi = pirate.akumaNoMi;
+    this.haki = pirate.haki;
+    this.weapons = pirate.weapons;
+    this.age = pirate.age;
+    this.gender = pirate.gender;
+    this.height = pirate.height;
+    this.personality = pirate.personality;
+    this.goal = pirate.goal;
+  }
+}
+
+export default Pirate;

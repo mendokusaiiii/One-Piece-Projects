7901a6a9e862ace850179dd627888b1e9936c0e7
Victor Cabral <victor_cabral_o@hotmail.com>
2023-06-22 16:40:20 -0300

    feat: Pirate Interface

---
+++ src/Interfaces/IPirate.ts
@@ -0,0 +1,14 @@
+export default interface IPirate {
+  name: string;
+  alias?: string;
+  crew: string;
+  bounty: number;
+  akumaNoMi?: string;
+  haki?: string[];
+  weapons?: string;
+  age: number;
+  gender: string;
+  height: string;
+  personality: string;
+  goal: string;
+}

ee1d53d51f35c123b7ad51a00fba74ec3e69ec8b
Victor Cabral <victor_cabral_o@hotmail.com>
2023-06-23 15:19:06 -0300

    feat: Interface for marines

---
+++ src/Interfaces/IMarine.ts
@@ -0,0 +1,15 @@
+export default interface IMarine {
+  id?: string;
+  name: string;
+  alias?: string;
+  rank: string;
+  affiliation: string;
+  akumaNoMi?: string;
+  haki?: string[];
+  weapons?: string;
+  age: number;
+  gender: string;
+  height: string;
+  personality: string;
+  goal: string;
+}

8c9db2967d1a9a26b0ff0f277f4082a4a5248533
Victor Cabral <victor_cabral_o@hotmail.com>
2023-06-28 16:21:01 -0300

    feat: constant numbers

---
+++ src/Constants/Number.ts
@@ -0,0 +1,10 @@
+class Number {
+  public static readonly USERNAME_MIN_LENGTH: number = 3
+  public static readonly USERNAME_MAX_LENGTH: number = 20
+  public static readonly NAME_MIN_LENGTH: number = 3
+  public static readonly NAME_MAX_LENGTH: number = 80
+  public static readonly EMAIL_MAX_LENGTH: number = 50
+  public static readonly PASSWORD_MIN_LENGTH: number = 8
+}
+
+export default Number;

032aed8d1c2242d1a3e0f93e2b83fde3e120b44a
Victor Cabral <victor_cabral_o@hotmail.com>
2023-06-29 15:52:27 -0300

    feat: login domain

--- src/database/Domains/Login.ts
+++ src/database/Domains/Login.ts
@@ -4,11 +4,15 @@ class Login {
   protected username: string;
   protected password: string;
   protected email: string;
+  protected name: string;
+  protected isAdmin: boolean;
   
   constructor(login: ILogin) {
     this.username = login.username;
     this.password = login.password;
     this.email = login.email;
+    this.name = login.name;
+    this.isAdmin = login.isAdmin;
   }
 }
 

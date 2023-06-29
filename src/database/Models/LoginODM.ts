271d3df4baf2d182ed2f7cf3df7c888fbeca36b8
Victor Cabral <victor_cabral_o@hotmail.com>
2023-06-29 15:52:38 -0300

    feat: login model

--- src/database/Models/LoginODM.ts
+++ src/database/Models/LoginODM.ts
@@ -1,16 +1,12 @@
-import { Schema } from 'mongoose';
-import AbstractODM from './AbstractODM';
-import ILogin from '../../Interfaces/ILogin';
+import mongoose from 'mongoose';
 
-class LoginODM extends AbstractODM<ILogin> {
-  constructor() {
-    const schema = new Schema<ILogin>({
-      username: { type: String, required: true },
-      password: { type: String, required: true },
-      email: { type: String, required: true },
-    });
-    super(schema, 'Login');
-  }
-}
+import LoginSchema from '../../Schemas/LoginSchema';
+import ILogin from '../../Interfaces/ILogin';
+import Model from '../../Constants/Model';
+
+const LoginODM = mongoose.model<ILogin>(
+  Model.USER_MODEL,
+  LoginSchema,
+);
 
 export default LoginODM;

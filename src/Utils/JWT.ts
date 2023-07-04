d050412d80a9689e0b9e0a438a048a07b5266f8d
Victor Cabral <victor_cabral_o@hotmail.com>
2023-07-04 17:25:55 -0300

    feat: added fuctions on JWT

--- src/Utils/JWT.ts
+++ src/Utils/JWT.ts
@@ -7,8 +7,10 @@ import {
   SignOptions,
   verify,
 } from 'jsonwebtoken';
+import CryptoJS from 'crypto-js';
 import IJWT from '../Interfaces/IJWT';
 import ErrorHandler from './ErrorHandler';
+import Variable from '../Env/Variable';
 
 class JWT implements IJWT {
   private _secret: Secret;
@@ -36,6 +38,18 @@ class JWT implements IJWT {
     } catch (_e) {
       throw new ErrorHandler(401, 'Token must be a valid token');
     }
+  }
+
+  public encrypt(password: string): string {
+    return CryptoJS.AES.encrypt(password, Variable.PASS_SECRET).toString();
+  }
+
+  public decrypt(password: string): string {
+    return CryptoJS.AES.decrypt(password, Variable.PASS_SECRET).toString(CryptoJS.enc.Utf8);
+  }
+
+  public comparePassword(password: string, decryptedPassword: string): boolean {
+    return password === this.decrypt(decryptedPassword);
   }
 }
 
